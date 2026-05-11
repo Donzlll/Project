ce87294a50d33846d018534139faeb489ee7e640
Donzlll <mojicabrandon48@gmail.com>
2026-05-12 05:23:45 +0800

    Main Data

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import sqlite3
-conn = sqlite3.connect('database.db')
+conn = sqlite3.connect('database.db') #Creates/opens database file
 c = conn.cursor()
+
+c.execute("DROP TABLE IF EXISTS users") # Delete the existing data
 
 #create a table
 c.execute('''
     CREATE TABLE IF NOT EXISTS users (
         Student_ID INTEGER PRIMARY KEY AUTOINCREMENT,
-        Name TEXT NOT NULL,
-        Age INTEGER NOT NULL,
+        Student_Name TEXT NOT NULL,
+        Student_Age INTEGER NOT NULL,
         Course TEXT NOT NULL,
-        Year INTEGER NOT NULL
+        Year_Level INTEGER NOT NULL
     )
 ''')
-conn.commit()
-
-Demodata = [
+conn.commit() #must commit to save changes
+#multiple data
+testdata = [
     ('Michael Reyes', 19, 'BSIT', 1),     # Name, Age, Course, Year
     ('Ana Santos', 20, 'BSCS', 2),
     ('Juan Cruz', 18, 'BSIT', 1),
@@ -22,8 +24,9 @@ Demodata = [
 ]
 
 c.executemany("""
-    INSERT INTO users (Name, Age, Course, Year) 
+    INSERT INTO users (Student_Name, Student_Age, Course, Year_Level) 
     VALUES (?, ?, ?, ?)
-""", Demodata)
+""", testdata)
+
 conn.commit()
 conn.close()
