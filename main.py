f4cd9b13640f48ea4d9d945d2974d4b2f90c40e2
Donzlll <mojicabrandon48@gmail.com>
2026-05-12 01:37:47 +0800

    Database

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-print("Hello World")
-print("Firts Trial")
-print("Second Trial")
-print("Third Trial")
-print("Fourth Trial")
+import sqlite3
+
+prof = {
+    "Name": "Michael Reyes",
+    "Student_ID": "25-1018",
+    "Course": "BSIT",
+    "Section": "BSIT 1-1",
+    "Age": 19,
+    "Gender": "Male"
+}
+
+conn = sqlite3.connect('database.db')
+# PERFECT CREATE TABLE (6 columns)
+conn.execute('''CREATE TABLE profile (
+    Name TEXT,
+    Student_ID TEXT,
+    Course TEXT,
+    Section TEXT,
+    Age INTEGER,
+    Gender TEXT
+)''')
+
+# PERFECT INSERT (6 values)
+conn.execute('''INSERT INTO profile VALUES (?,?,?,?,?,?)''',
+             (prof["Name"],
+              prof["Student_ID"],
+              prof["Course"],
+              prof["Section"],
+              prof["Age"],
+              prof["Gender"]))
+
+conn.commit()
+conn.close()
